a129ce5267675f3919eff2c8c8751d77329fa6e0
Probir Ghosh <probirghosh@outlook.com>
2024-07-26 13:40:57 +0600

    navbar added :tada:

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <Container>
        <Stack py={2} direction="row" justifyContent="space-between" >
-        <Typography variant="h4" component="h1" fontWeight={600}>
+        <Typography variant="h4" component="h1" fontWeight={600}> 
           P<Box component="span" color="primary.main" >H</Box> HealthCare
         </Typography>
        </Stack>
